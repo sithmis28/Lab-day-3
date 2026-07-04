1199878e16d04d8af3cd2edd377213f9d6e98da4
Sithmi Sanjana <saachis30@gmail.com>
2026-07-04 10:42:22 +0530

    Add files via upload

---
+++ lab 3.2.py
@@ -0,0 +1,15 @@
+#inputs
+a=float(input("enter your 1st marks:"))
+b=float(input("enter your 2nd marks:"))
+c=float(input("enter your 3rd marks:"))
+d=float(input("enter your 4rd marks:"))
+
+#calculation
+total=a+b+c+d
+avg=total/4
+GPA=avg/25     #SIMPLE GPA FORMULA:100 MARKS = 4.0 GPA
+
+#output
+print("total is",total)
+print("avg is",avg)
+print("GPA is",GPA)
